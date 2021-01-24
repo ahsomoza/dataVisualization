7b232ba72037faf068f677a9a173607a33f32ed5
ahsomoza <upm272523>
2021-01-24 22:12:03 +0100

    remove auxiliar

--- AppAirBnB/auxiliar.R
+++ AppAirBnB/auxiliar.R
@@ -9,3 +9,10 @@ df$date <- as.Date(df$Group.1)
 df$price <- as.numeric(df$x)
 df <- df[,c("date", "price")]
 write.csv(df, "./data/calendarMeanPrice.csv")
+
+
+rsconnect::setAccountInfo(name='group17mds',
+                          token='C5859B799DC1C1DC35A30169C61150F8',
+                          secret='qMIAQQ2jZN6mD7naDT58oFCqudWkdcgdKK6pi0O4')
+
+rsconnect::deployApp('/home/alberto/Documentos/Repos/GitHub/dataVisualization/AppAirBnB')
